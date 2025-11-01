3ff9b50a78ef01d9c2f4fb8c7e896dd4af8268ee
PRASHANT PAREKH <prashantparekh1501@gmail.com>
2025-11-02 02:18:26 +0530

    Polymorphism

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,17 @@
 package com.driver;
 
 public class Main {
+   public static void main(String[] args) {
+      Product p = new Product();
 
+      int prod2 = p.product(2, 3);
+      System.out.println("product of 2 numbers :" + prod2);
+
+      int prod3 = p.product(4,5,6);
+      System.out.println("product of 3 numbers :" + prod3);
+
+      double prod4 = p.product(2.3,3.4);
+      System.out.println("product of 2 numbers :" + prod4);
+
+   }
 }
