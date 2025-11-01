e346ecf1e624d0327046936517bfa0452faca82c
PRASHANT PAREKH <prashantparekh1501@gmail.com>
2025-11-02 02:31:55 +0530

    Polymorphism done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,9 +4,12 @@ public class Main {
    public static void main(String[] args) {
       Product p = new Product();
 
-      p.product(2,3);
-      p.product(2,3,4);
-      p.product(2.5,2.5);
+      int res1 = p.product(2, 3);           // 2 integers
+      int res2 = p.product(2, 3, 4);        // 3 integers
+      double res3 = p.product(2.5, 2.5);    // 2 doubles
 
+      System.out.println(res1);
+      System.out.println(res2);
+      System.out.println(res3);
    }
 }
